ee27712ee87835b9d3b7bfc4dccbba7285aad604
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-04-11 18:34:13 +0300

    Initial commit

---
+++ src/main/java/com/example/warlock/WarlockApplication.java
@@ -0,0 +1,13 @@
+package com.example.warlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarlockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/warlock/WarlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
