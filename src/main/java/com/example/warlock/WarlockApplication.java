d10476be2e3a213ff83d452461c63292728a2eca
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-04-11 18:35:20 +0300

    add

--- src/main/java/com/example/warlock/WarlockApplication.java
+++ src/main/java/com/example/warlock/WarlockApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WarlockApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WarlockApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WarlockApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/warlock/WarlockApplicationTests.java
+++ src/test/java/com/example/warlock/WarlockApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WarlockApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
